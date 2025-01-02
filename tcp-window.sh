0736b5e6df0741bc13eef629837b7e14f3ae932e
liuyisi-afk <83866632+liuyisi-afk@users.noreply.github.com>
2025-01-02 15:45:05 +0800

    Add files via upload

--- tcp-window.sh
+++ tcp-window.sh
@@ -6,45 +6,8 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-# 清空原有的 sysctl 配置，避免重复添加
-sed -i '/kernel.msgmnb/d' /etc/sysctl.conf
-sed -i '/kernel.msgmax/d' /etc/sysctl.conf
-sed -i '/kernel.shmmax/d' /etc/sysctl.conf
-sed -i '/kernel.shmall/d' /etc/sysctl.conf
-sed -i '/vm.swappiness/d' /etc/sysctl.conf
-sed -i '/net.core.rps_sock_flow_entries/d' /etc/sysctl.conf
-sed -i '/fs.file-max/d' /etc/sysctl.conf
-sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_retries1/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_retries2/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_orphan_retries/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_max_orphans/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_syn_retries/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_synack_retries/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
-sed -i '/net.core.netdev_budget/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
-sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
-sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_notsent_lowat/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_keepalive_time/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_keepalive_probes/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_keepalive_intvl/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_window_scaling/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_adv_win_scale/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_moderate_rcvbuf/d' /etc/sysctl.conf
-sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
-sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
-sed -i '/net.ipv4.udp_rmem_min/d' /etc/sysctl.conf
-sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_mem/d' /etc/sysctl.conf
-sed -i '/net.ipv4.udp_mem/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
-sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
-sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+# 删除 /etc/sysctl.conf 文件中的所有内容
+> /etc/sysctl.conf
 
 # 重新写入新的配置到 /etc/sysctl.conf
 cat <<EOF >> /etc/sysctl.conf
